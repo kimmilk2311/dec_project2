004f346f6414e0282e9dc858f649187a8d41a931
kimmilk2311 <kim244969@gmail.com>
2024-05-18 20:48:04 +0700

    3

--- models/staging/stg_dim_customer.sql
+++ models/staging/stg_dim_customer.sql
@@ -1,55 +1,55 @@
-WITH dim_customer_source as (
-SELECT * 
-FROM `adventureworks2019.Sales.Customer`
+WITH dim_customer_source AS (
+    SELECT *
+    FROM `adventureworks2019.Sales.Customer`
 )
-
-,dim_customer_rename as (
-SELECT 
-    CustomerID as customer_id
-    ,PersonID as person_id
-    ,StoreID as store_id
-FROM dim_customer_source
+,dim_customer_rename AS (
+    SELECT 
+        CustomerID AS customer_id,
+        PersonID AS person_id,
+        StoreID AS store_id
+    FROM dim_customer_source
 )
-
-,dim_customer_handel_null as (
-SELECT 
-    customer_id,
-    CASE
-       WHEN person_id = 'NULL' THEN NULL
-       ELSE person_id
-    END as person_id,
-    CASE
-       WHEN store_id = 'NULL' THEN NULL 
-       ELSE store_id
-    END as store_id
-FROM dim_customer_rename
+,dim_customer_handle_null AS (
+    SELECT 
+        customer_id,
+        CASE
+           WHEN person_id = 'NULL' THEN NULL
+           ELSE person_id
+        END AS person_id,
+        CASE
+           WHEN store_id = 'NULL' THEN NULL 
+           ELSE store_id
+        END AS store_id
+    FROM dim_customer_rename
 )
-
-,dim_customer_cast_type as (
-SELECT 
-    cast(customer_id as integer) as customer_id
-    ,cast(person_id as integer) as person_id
-    ,cast(store_id as integer) as store_id
-FROM dim_customer_handel_null
+,dim_customer_cast_type AS (
+    SELECT 
+        CAST(customer_id AS INTEGER) AS customer_id,
+        CAST(person_id AS INTEGER) AS person_id,
+        CAST(store_id AS INTEGER) AS store_id
+    FROM dim_customer_handle_null
 )
+,dim_customer_undefined_record AS (
+    SELECT 
+       customer_id,
+       person_id,
+       store_id,
+       CASE 
+         WHEN store_id IS NOT NULL THEN 'reseller'
+         ELSE 'customer'
+       END AS is_reseller
+    FROM dim_customer_cast_type
 
-,dim_customer_underfined_record as (
-SELECT 
-   customer_id
-   ,person_id 
-   ,store_id
-FROM dim_customer_cast_type
-
-UNION ALL
-
+    UNION ALL
+    
+    SELECT
+       0 AS customer_id,
+       0 AS person_id,
+       0 AS store_id,
+       'Undefined' AS is_reseller
+)
 SELECT
-   0 as customer_id
-   ,0 as person_id
-   ,0 as store_id
-)
-
-SELECT
    customer_id
-   ,person_id 
-   ,store_id
-FROM dim_customer_underfined_record
+   ,person_id
+   ,is_reseller
+FROM dim_customer_undefined_record
